b1dcc5e0145403de89f7c0dfde4ace8f552a328e
MikeB <burton.mj@gmail.com>
2017-02-24 12:29:16 -0700

    mikes first commit with changes

---
+++ HTML/Angular/app.js
@@ -0,0 +1,26 @@
+angular.module('physicianPortal', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('welcome', {
+    url: '/',
+    templateUrl: 'js/welcome/welcome.html',
+  })
+  .state('outstandingreports', {
+    url: '/outstandingreports',
+    templateUrl: 'js/reports/outstandingReports.html',
+  })
+  .state('completedreports', {
+    url: '/completedreports',
+    templateUrl: 'js/reports/completedReports.html',
+  })
+  .state('patientlist', {
+    url: '/patientlist',
+    templateUrl: 'js/patientList/patientList.html',
+  })
+
+
+
+  $urlRouterProvider
+      .otherwise('/')
+
+})
